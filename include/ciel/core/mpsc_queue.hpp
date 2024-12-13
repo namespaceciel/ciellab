585925d2c61e3e84f0b51f64f3608746e8f569f4
ciel <createdbycrt@gmail.com>
2024-12-13 17:58:39 +0800

    mpsc_queue implementation

---
+++ include/ciel/core/mpsc_queue.hpp
@@ -0,0 +1,108 @@
+#ifndef CIELLAB_INCLUDE_CIEL_CORE_MPSC_QUEUE_HPP_
+#define CIELLAB_INCLUDE_CIEL_CORE_MPSC_QUEUE_HPP_
+
+#include <ciel/core/aligned_storage.hpp>
+#include <ciel/core/config.hpp>
+#include <ciel/core/message.hpp>
+
+#include <atomic>
+#include <type_traits>
+
+NAMESPACE_CIEL_BEGIN
+
+template<class T>
+class mpsc_queue {
+    static_assert(std::is_same<decltype(T::next), std::atomic<T*>>::value, "");
+
+private:
+    alignas(cacheline_size) std::atomic<T*> front_{nullptr};
+    alignas(cacheline_size) std::atomic<T*> back_{nullptr};
+    aligned_storage<sizeof(T), alignof(T)> stub_;
+
+public:
+    mpsc_queue() noexcept {
+        T* stub_ptr      = reinterpret_cast<T*>(&stub_);
+        using AtomicTPtr = std::atomic<T*>;
+        ::new (&(stub_ptr->next)) AtomicTPtr(nullptr);
+        front_.store(stub_ptr, std::memory_order_relaxed);
+    }
+
+    mpsc_queue(const mpsc_queue&)            = delete;
+    mpsc_queue& operator=(const mpsc_queue&) = delete;
+
+    void push(T* t) noexcept {
+        push(t, t);
+    }
+
+    void push(T* first, T* last) noexcept {
+        CIEL_PRECONDITION(first != nullptr);
+        CIEL_PRECONDITION(last != nullptr);
+
+        last->next.store(nullptr, std::memory_order_relaxed);
+        // It needs to be release, so nullptr in next is visible, and needs to be acquire,
+        // so linking into the list does not race with other threads nullptr-initing of the next field.
+        T* prev = back_.exchange(last, std::memory_order_acq_rel);
+
+        if CIEL_LIKELY (prev != nullptr) { // not at stub state
+            prev->next.store(first, std::memory_order_relaxed);
+            return;
+        }
+
+        // at stub state, remove stub
+        front_.store(first, std::memory_order_relaxed);
+    }
+
+    // ProcessEachNode is a monadic predicate callback type, to properly process each node.
+    // The callback may return false to stop the iteration early, but must have processed the element it was given.
+    template<class ProcessEachNode>
+    void process(ProcessEachNode&& process_each_node) noexcept {
+        T* cur = front_.load(std::memory_order_relaxed);
+        T* b   = back_.load(std::memory_order_relaxed);
+
+        if CIEL_UNLIKELY (b == nullptr) { // at stub state
+            return;
+        }
+
+        while (cur != b) {
+            T* next = cur->next.load(std::memory_order_relaxed);
+
+            // If the push is happening halfway (back_ is updated while prev->next is not updated yet).
+            if CIEL_UNLIKELY (next == nullptr) {
+                break;
+            }
+
+            if CIEL_UNLIKELY (!process_each_node(cur)) {
+                front_.store(next, std::memory_order_relaxed);
+                return;
+            }
+
+            cur = next;
+        }
+
+        // Probably only one node left.
+        front_.store(cur, std::memory_order_relaxed);
+    }
+
+    // ProcessEachNode callback must process all nodes left.
+    // No other related threads shall exist here.
+    template<class ProcessEachNode>
+    void destructive_process(ProcessEachNode&& process_each_node) noexcept {
+        T* cur = front_.load(std::memory_order_relaxed);
+        T* b   = back_.load(std::memory_order_relaxed);
+
+        if CIEL_UNLIKELY (b == nullptr) { // at stub state
+            return;
+        }
+
+        while (cur != nullptr) {
+            T* next = cur->next.load(std::memory_order_relaxed);
+            process_each_node(cur);
+            cur = next;
+        }
+    }
+
+}; // class mpsc_queue
+
+NAMESPACE_CIEL_END
+
+#endif // CIELLAB_INCLUDE_CIEL_CORE_MPSC_QUEUE_HPP_
---
+++ test/src/mpsc_queue.cpp
@@ -0,0 +1,104 @@
+#include <gtest/gtest.h>
+
+#include <ciel/core/message.hpp>
+#include <ciel/core/mpsc_queue.hpp>
+#include <ciel/test/simple_latch.hpp>
+#include <ciel/vector.hpp>
+
+#include <array>
+#include <atomic>
+#include <cstddef>
+#include <memory>
+#include <thread>
+
+using namespace ciel;
+
+namespace {
+
+struct Node {
+    size_t value{1};
+    std::atomic<Node*> next{this}; // Intentionally
+
+}; // struct Node
+
+} // namespace
+
+TEST(mpsc_queue, singlethread) {
+    std::array<Node, 6> arr;
+    mpsc_queue<Node> queue;
+    std::atomic<size_t> count{0};
+    queue.process([&](Node*) {
+        CIEL_PRECONDITION(false);
+        return true;
+    });
+
+    queue.push(arr.data());
+    queue.process([&](Node*) {
+        CIEL_PRECONDITION(false);
+        return true;
+    });
+
+    (arr.data() + 1)->next.store(arr.data() + 2);
+    (arr.data() + 2)->next.store(arr.data() + 3);
+    (arr.data() + 3)->next.store(arr.data() + 4);
+    (arr.data() + 4)->next.store(arr.data() + 5);
+    queue.push(arr.data() + 1, arr.data() + 5);
+    queue.process([&](Node* node) {
+        count += node->value;
+        return true;
+    });
+    ASSERT_EQ(count, 5);
+
+    queue.destructive_process([&](Node* node) {
+        count += node->value;
+    });
+    ASSERT_EQ(count, 6);
+}
+
+TEST(mpsc_queue, multithread) {
+    constexpr size_t producer_threads_num = 64;
+    constexpr size_t operations_num       = 1000;
+
+    std::array<std::array<Node, operations_num>, producer_threads_num> arr;
+    SimpleLatch go{producer_threads_num + 1};
+    mpsc_queue<Node> queue;
+    std::atomic<size_t> count{0};
+
+    ciel::vector<std::thread> producer_threads;
+    producer_threads.reserve(producer_threads_num);
+    for (size_t i = 0; i < producer_threads_num; ++i) {
+        producer_threads.unchecked_emplace_back([&, i] {
+            go.arrive_and_wait();
+
+            for (size_t j = 0; j < operations_num; ++j) {
+                queue.push(std::addressof(arr[i][j]));
+            }
+        });
+    }
+
+    std::atomic<bool> flag{false};
+    std::thread consumer([&] {
+        go.arrive_and_wait();
+
+        while (!flag) {
+            queue.process([&](Node* node) {
+                count += node->value;
+                return true;
+            });
+
+            std::this_thread::yield();
+        }
+    });
+
+    for (auto& t : producer_threads) {
+        t.join();
+    }
+    flag = true;
+    consumer.join();
+
+    queue.destructive_process([&](Node* node) {
+        count += node->value;
+    });
+
+    ASSERT_EQ(count, producer_threads_num * operations_num);
+}
